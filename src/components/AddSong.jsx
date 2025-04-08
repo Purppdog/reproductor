9e0ba0d02106baf7e8f8a6786e9d8566576edf42
Purppdog <asqw7858@gmail.com>
2025-04-08 15:44:31 -0500

    Fix de errores

--- src/components/AddSong.jsx
+++ src/components/AddSong.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import "../styles/addsong.css";
+import "../styles/components/AddSong.css"
 
 export default function AddSong({ onSongAdded, onClose }) {
     const [title, setTitle] = useState("");
